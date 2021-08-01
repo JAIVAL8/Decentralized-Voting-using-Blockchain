8a3bc220ecba3222510ae6092f48e118f9cbea5a
rahul lanjewar <rahullanjewar93@gmail.com>
2021-08-01 16:56:47 +0530

    fix(navbar):fixed responsive issue at navbar

--- server/client/src/components/Navbar.js
+++ server/client/src/components/Navbar.js
@@ -5,30 +5,48 @@ import 'bootstrap/dist/css/bootstrap.css'
 
 function Navbar() {
   return (
-          
-  <nav class="navbar navbar-expand-lg navbar-light bg-light">
-    <div class="container-fluid">
-      <a class="navbar-brand" href="#"><h1>F-votechain</h1></a>
-      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+  <div>
+      <nav class="navbar navbar-expand-lg navbar-light bg-light">
+      <a class="navbar-brand" href="#">Navbar</a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
+
       <div class="collapse navbar-collapse" id="navbarSupportedContent">
-        <ul class="navbar-nav ml-auto mb-2 mb-lg-0">
-          <li class="nav-item">
-            <a class="nav-link" href="/Login">Login</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="/Signup">SignUp</a>
-          </li> 
-        </ul>  
+        <ul class="navbar-nav mr-auto">
+        <li className="nav-item">
+             <a className="nav-link" href="/Login">Login</a>
+           </li>
+           <li className="nav-item">
+             <a className="nav-link" href="/Signup">SignUp</a>
+           </li>
+        </ul>
       </div>
-    </div>
-   </nav>
-  );
+    </nav>
+  </div>
+  )
 }
 
 export default Navbar;
-
+  // <nav className="navbar navbar-expand-lg navbar-light bg-light">
+  //   <div className="container-fluid">
+  //     <a className="navbar-brand" href="#"><h1>F-votechain</h1></a>
+  //     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+  //       <span className="navbar-toggler-icon"></span>
+  //     </button>
+  //     <div className="collapse navbar-collapse" id="navbarSupportedContent">
+  //       <ul className="navbar-nav ml-auto mb-2 mb-lg-0">
+  //         <li className="nav-item">
+  //           <a className="nav-link" href="/Login">Login</a>
+  //         </li>
+  //         <li className="nav-item">
+  //           <a className="nav-link" href="/Signup">SignUp</a>
+  //         </li>
+  //       </ul>
+  //     </div>
+  //   </div>
+  //  </nav>
+  // );
 /*
 <div>
       <nav>
