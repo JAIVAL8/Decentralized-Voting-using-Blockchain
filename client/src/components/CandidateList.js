6917e103dbb60906fcadd2f75d179b6e9df1ea58
Jaivalp <jaivalapatel@gmail.com>
2021-11-10 00:49:35 +0530

    test

--- client/src/components/CandidateList.js
+++ client/src/components/CandidateList.js
@@ -1,4 +1,4 @@
-import React, { useState, useContext } from "react";
+import React, { useEffect, useState, useContext } from "react";
 import "./CandidateList.css";
 import AAP from "../images/aap.png";
 import Congress from "../images/Congress.jpg";
@@ -13,9 +13,32 @@ function CandidateList() {
   const { state, dispatch } = useContext(UserContext);
   const history = useHistory();
   const [disable, setDisable] = useState(false);
+  // useEffect(() => {
+  //   const user = JSON.parse(localStorage.getItem("user"));
+  //   var uId = user.uId;
+  //   fetch("http://localhost:4001/transaction/broadcast", {
+  //     method: "post",
+
+  //     headers: {
+  //       "Content-Type": "application/json",
+  //     },
+  //     body: JSON.stringify({
+  //       uid: uId,
+  //     }),
+  //   })
+  //     .then((res) => res.json())
+  //     .then((data) => {
+  //       if (data.error) {
+  //         toast.error(data.error, {
+  //           position: "top-center",
+  //         });
+  //         setDisable(true);
+  //       }
+  //     });
+  // }, []);
 
   const postData = (candidateName) => {
-    //console.log(candidateName);
+    console.log(candidateName);
     const user = JSON.parse(localStorage.getItem("user"));
 
     swal({
@@ -96,7 +119,6 @@ function CandidateList() {
                   e.confirm(value)
                     .then((res) => {
                       //console.log("===>>>", res);
-                      
 
                       fetch("/send-mail", {
                         method: "post",
@@ -113,51 +135,50 @@ function CandidateList() {
                       });
                       //dispatch({ type: "VOTE", payload: { voted: true } });
                       // console.log(state);
-                      setDisable(true);
-                      const {uId,city, gender, age} =user;
-                      
-                      const  receiver = candidateName;
+
+                      const { uId, city, gender, age } = user;
+
+                      const receiver = candidateName;
                       console.log(uId);
-                 fetch('http://localhost:4001/transaction/broadcast', {
-                  
-                 method: "post",
-                 
-                  headers: {
-                    "Content-Type": "application/json",
-                    
-                  },
-                  body:JSON.stringify ({
-                  uid:uId,receiver:candidateName,location:city,age:age,gender:gender
-                   
-                  }),
-                  
-                })
-                  .then((res) => res.json())
-                  .then((data) => {
-                    if (data.error) {
-                      toast.error(data.error, {
-                        position: "top-center",
-                      });
-                     
-                      return;
-                    }
-                    else{
-                    console.log(data);
-                    swal(
-                      "🎉✨",
-                      "Voted to " +
-                        candidateName +
-                        " successully. Check your email",
-                      "success"
-                    );
-                    window.setTimeout(() => {
-                      history.push("/dashboard");
-                    }, 1700);
-                    } }
-                      ).catch((err)=>{console.log(err)});
-               
-                  
-                      
+                      fetch("http://localhost:4001/transaction/broadcast", {
+                        method: "post",
+
+                        headers: {
+                          "Content-Type": "application/json",
+                        },
+                        body: JSON.stringify({
+                          uid: uId,
+                          receiver: candidateName,
+                          location: city,
+                          age: age,
+                          gender: gender,
+                        }),
+                      })
+                        .then((res) => res.json())
+                        .then((data) => {
+                          if (data.error) {
+                            toast.error(data.error, {
+                              position: "top-center",
+                            });
+
+                            return;
+                          } else {
+                            console.log(data);
+                            swal(
+                              "🎉✨",
+                              "Voted to " +
+                              candidateName +
+                              " successully. Check your email",
+                              "success"
+                            );
+                            window.setTimeout(() => {
+                              history.push("/dashboard");
+                            }, 1700);
+                          }
+                        })
+                        .catch((err) => {
+                          console.log(err);
+                        });
                     })
                     .catch((err) => {
                       toast.error("*OTP* does not match!", {
@@ -194,11 +215,10 @@ function CandidateList() {
     <>
       <section id="facilities">
         <div className="title">
-          <h1>Candidate's List</h1>
+          <h1> Candidate 's List</h1>{" "}
         </div>
-
         <div className="containers">
-          <div id="recaptcha-container"></div>
+          <div id="recaptcha-container"> </div>{" "}
           <div
             className="row gy-3"
             style={{
@@ -209,125 +229,155 @@ function CandidateList() {
             }}
           >
             <div className="col-sm-12 col-md-6 col-lg-4 py-3">
-              <div className="card" style={{ width: "18rem" }}>
+              <div
+                className="card"
+                style={{
+                  width: "18rem",
+                }}
+              >
                 <div className="inner">
-                  <img className="card-img-top" src={Congress} alt="Card"></img>
-                </div>
+                  <img className="card-img-top" src={Congress} alt="Card">
+                    {" "}
+                  </img>{" "}
+                </div>{" "}
                 <div className="card-body text-center">
-                  <h5 className="card-title">CONGRESS</h5>
+                  <h5 className="card-title"> CONGRESS </h5>{" "}
                   <p className="card-text">
                     Rahul Gandhi is an Indian politician and a member of the
-                    Indian Parliament,constituency of Wayanad, Kerala in 17th
-                    Lok Sabha.
+                    Indian Parliament, constituency of Wayanad, Kerala in 17 th
+                    Lok Sabha.{" "}
                   </p>
-
                   <button
                     className="btn btn-primary"
                     disabled={disable}
                     onClick={() => postData("CONGRESS")}
                   >
-                    Click to Vote
-                  </button>
-                </div>
-              </div>
-            </div>
+                    Click to Vote{" "}
+                  </button>{" "}
+                </div>{" "}
+              </div>{" "}
+            </div>{" "}
             <div className="col-sm-12 col-md-6 col-lg-4 py-3">
-              <div className="card" style={{ width: "18rem" }}>
+              <div
+                className="card"
+                style={{
+                  width: "18rem",
+                }}
+              >
                 <div className="inner">
-                  <img className="card-img-top" src={AAP} alt="Card"></img>
-                </div>
+                  <img className="card-img-top" src={AAP} alt="Card">
+                    {" "}
+                  </img>{" "}
+                </div>{" "}
                 <div className="card-body text-center">
-                  <h5 className="card-title">AAP</h5>
+                  <h5 className="card-title"> AAP </h5>{" "}
                   <p className="card-text">
                     Arvind Kejriwal is an Indian politician and a former
-                    bureaucrat who is the current and 7th Chief Minister of
-                    Delhi since February 2015.
-                  </p>
+                    bureaucrat who is the current and 7 th Chief Minister of
+                    Delhi since February 2015.{" "}
+                  </p>{" "}
                   <button
                     className="btn btn-primary"
                     disabled={disable}
                     onClick={() => postData("AAP")}
                   >
-                    Click to Vote
-                  </button>
-                </div>
-              </div>
-            </div>
+                    Click to Vote{" "}
+                  </button>{" "}
+                </div>{" "}
+              </div>{" "}
+            </div>{" "}
             <div className="col-sm-12 col-md-6 col-lg-4 py-3">
-              <div className="card" style={{ width: "18rem" }}>
+              <div
+                className="card"
+                style={{
+                  width: "18rem",
+                }}
+              >
                 <div className="inner">
                   <img
                     className="card-img-top"
                     src="https://upload.wikimedia.org/wikipedia/commons/thumb/1/1e/Bharatiya_Janata_Party_logo.svg/1200px-Bharatiya_Janata_Party_logo.svg.png"
                     alt="Card"
-                  ></img>
-                </div>
+                  ></img>{" "}
+                </div>{" "}
                 <div className="card-body text-center">
-                  <h5 className="card-title">BJP</h5>
+                  <h5 className="card-title"> BJP </h5>{" "}
                   <p className="card-text">
                     Narendra Damodardas Modi is an Indian politician serving as
-                    the 14th and current prime minister of India since 2014.
-                  </p>
+                    the 14 th and current prime minister of India since 2014.{" "}
+                  </p>{" "}
                   <button
                     className="btn btn-primary"
                     disabled={disable}
                     onClick={() => postData("BJP")}
                   >
-                    Click to Vote
-                  </button>
-                </div>
-              </div>
-            </div>
+                    Click to Vote{" "}
+                  </button>{" "}
+                </div>{" "}
+              </div>{" "}
+            </div>{" "}
             <div className="col-sm-12 col-md-6 col-lg-4 py-3">
-              <div className="card" style={{ width: "18rem" }}>
+              <div
+                className="card"
+                style={{
+                  width: "18rem",
+                }}
+              >
                 <div className="inner">
                   <img
                     className="card-img-top"
                     src="https://timesofindia.indiatimes.com/thumb/msid-80876920,imgsize-57317,width-400,resizemode-4/80876920.jpg"
                     alt="Card"
-                  ></img>
-                </div>
+                  ></img>{" "}
+                </div>{" "}
                 <div className="card-body text-center">
-                  <h5 className="card-title">A.I.M.I.M</h5>
+                  <h5 className="card-title"> A.I.M.I.M </h5>{" "}
                   <p className="card-text">
                     Asaduddin Owaisi is an Indian politician, who is the
-                    President of the All India Majlis-e-Ittehadul Muslimeen.
-                  </p>
+                    President of the All India Majlis - e - Ittehadul Muslimeen.{" "}
+                  </p>{" "}
                   <button
                     className="btn btn-primary"
                     disabled={disable}
                     onClick={() => postData("A.I.M.I.M")}
                   >
-                    Click to Vote
-                  </button>
-                </div>
-              </div>
-            </div>
+                    Click to Vote{" "}
+                  </button>{" "}
+                </div>{" "}
+              </div>{" "}
+            </div>{" "}
             <div className="col-sm-12 col-md-6 col-lg-4 py-3">
-              <div className="card" style={{ width: "18rem" }}>
+              <div
+                className="card"
+                style={{
+                  width: "18rem",
+                }}
+              >
                 <div className="inner">
-                  <img className="card-img-top" src={Nota} alt="Card"></img>
-                </div>
+                  <img className="card-img-top" src={Nota} alt="Card">
+                    {" "}
+                  </img>{" "}
+                </div>{" "}
                 <div className="card-body text-center">
-                  <h5 className="card-title">NOTA</h5>
+                  <h5 className="card-title"> NOTA </h5>{" "}
                   <p className="card-text">
                     "None of the above", or NOTA for short, also known as
                     "against all" or a "scratch" vote, is a ballot option in
-                    some jurisdictions or organizations.
-                  </p>
+                    some jurisdictions or organizations.{" "}
+                  </p>{" "}
                   <button
                     className="btn btn-primary"
                     disabled={disable}
                     onClick={() => postData("NOTA")}
                   >
-                    Click to Vote
-                  </button>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </section>
+                    Click to Vote{" "}
+                  </button>{" "}
+                </div>{" "}
+              </div>{" "}
+            </div>{" "}
+          </div>{" "}
+        </div>{" "}
+      </section>{" "}
       <ToastContainer />
     </>
   );
