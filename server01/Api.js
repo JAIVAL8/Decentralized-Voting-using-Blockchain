3e117cebf7f343cf046dc79b82669c01b1bcf6d6
Jaivalp <jaivalapatel@gmail.com>
2022-02-11 20:33:49 +0530

    Blockchain v2  with decentalized mining (originally from individual) and various other fixes.

--- server01/Api.js
+++ server01/Api.js
@@ -5,11 +5,19 @@ const bodyParser = require('body-parser');
 app.use(bodyParser.json());
 
 const Blockchain = require('./Mainchain');
+
 const votechain = new Blockchain();
 const port = process.argv[2];
 const reqPromise = require('request-promise');
+const res = require('express/lib/response');
 
 
+app.get('/mine', function (req, res) {
+    let newBlock=votechain.createBlock(false);
+    console.log(JSON.stringify(newBlock));
+    newBlock=  votechain.mineBlock(newBlock);
+    res.send(newBlock);
+});
 
 
 app.get('/blockchain', function (req, res) {
@@ -20,7 +28,7 @@ app.get('/result', function (req, res) {
     uid,receiver=votechain.Results()
     res.send( uid,receiver);
 });
-app.get('/Forcesave', function (req, res) {
+app.get('/Forcemine', function (req, res) {
     votechain.ForceTransactionBlock();
     res.json(
         {
@@ -28,12 +36,12 @@ app.get('/Forcesave', function (req, res) {
         }
     );
 });
-app.post('/broadcast-forcemine', function (req,res) {
+app.get('/broadcast/Forcemine', function (req,res) {
   votechain.ForceTransactionBlock();
   const requests = [];
   votechain.networkNodes.forEach(networkNode => {
       const requestOptions = {
-          uri: networkNode + '/Forcesave',
+          uri: networkNode + '/Forcemine',
           method: 'GET',
           json: true
       };
@@ -51,43 +59,34 @@ app.post('/broadcast-forcemine', function (req,res) {
       });
 });
 
-app.post('/transaction', function (req, res) {
-    const transaction = req.body;
-    
-     votechain.PendingTransactions(transaction);
-   
-    res.json(
-        {
-            message: `Transaction will be added to block.`
-        }
-    );
-});
-app.post('/SetdifficultyandMininglimits', function (req, res) {
+
+
+app.post('/Set-Parameters', function (req, res) {
     const diff = parseInt(req.body.diff);
     const max = parseInt(req.body.max);
-     votechain.difficulty=diff;
-    votechain.maxTransperblock=max;
+    votechain.difficulty=diff;
+    votechain.maxvotes=max;
     res.json(
         {
             message: `Difficulty set for a block.`
         }
     );
     console.log("chain diff:"+votechain.difficulty);
-    console.log("Max Transactions per Block:"+votechain.maxTransperblock);
+    console.log("Max Transactions per Block:"+votechain.maxvotes);
 });
 
-app.post('/broadcast-node-mininglimit', function (req, res) {
+app.post('/broadcast/Set-Parameters', function (req, res) {
     const diff = parseInt(req.body.diff);
     const max = parseInt(req.body.max);
-  
     
-
     votechain.difficulty=diff;
-    votechain.maxTransperblock=max;
+    votechain.maxvotes=max;
+    console.log("chain diff:"+votechain.difficulty);
+    console.log("Max Transactions per Block:"+votechain.maxvotes);
     const requests = [];
     votechain.networkNodes.forEach(networkNode => {
         const requestOptions = {
-            uri: networkNode + '/SetdifficultyandMininglimits',
+            uri: networkNode + '/Set-Parameters',
             method: 'POST',
             body: { diff: diff,max:max },
             json: true
@@ -105,20 +104,38 @@ app.post('/broadcast-node-mininglimit', function (req, res) {
             );
         });
 });
+app.post('/Postblock', function (req, res) {
+    const newBlock = req.body.newBlock;
+    
+    const extra=req.body.extra;
+    votechain.addBlock(newBlock);
+    votechain.pendingTransactions=[]; 
+    votechain.pendingTransactions.push(extra);
+});
 
+app.post('/Pending-votes', function (req, res) {
+    const Transactions = req.body;
+    
+    votechain.pendingTransactions.push(Transactions);
 
-app.post('/transaction/broadcast', function (req, res) {
+    res.json(
+        {
+            message: `Votes will be added to Pending List.`
+        }
+    );
+});
+
+app.post('/broadcast/Pending-votes', function (req, res) {
     
     const transaction= votechain.addTransactions(req.body.uid,
         req.body.receiver,
         req.body.location,
         req.body.age,
-        req.body.gender
-        );
-        console.log(transaction);
-
-        console.log(votechain.DoesVoteExist(transaction.uid))
+        req.body.gender);
+       
         console.log('checking if id exist');
+        console.log(votechain.DoesVoteExist(transaction.uid))
+       
     if(votechain.DoesVoteExist(transaction.uid)){
        //Alert('Vote Already Exist') ;
       
@@ -130,11 +147,15 @@ app.post('/transaction/broadcast', function (req, res) {
         );
     }
     else{
-        votechain.PendingTransactions(transaction );
+        let total=votechain.pendingTransactions.length;
+      
+        if(total<votechain.maxvotes){
+            votechain.pendingTransactions.push(transaction);
+        
         const requests = [];
         votechain.networkNodes.forEach(networkNode => {
             const requestOptions = {
-                uri: networkNode + '/transaction',
+                uri: networkNode + '/Pending-votes',
                 method: 'POST',
                 body: transaction,
                 json: true
@@ -147,12 +168,76 @@ app.post('/transaction/broadcast', function (req, res) {
             .then(data => {
                 res.json(
                     {
-                        message: `Creating and broadcasting Transaction successfully!`
+                        message: `Broadcasting votes successfully!`
                     }
                 );
             });
+        }
+        else{
+            console.log('Mining since pending list is full!!!') ;
+            votechain.pendingTransactions.push(transaction);
+           
+            const extra=votechain.pendingTransactions[votechain.maxvotes];
+            
+            const start = Date.now();
+            const requests = [];
+            Nodes=[];
+            Nodes=Nodes.concat(votechain.networkNodes);
+            Nodes.push(votechain.nodeUrl);
+            
+            Nodes.forEach(Node => {
+            const requestOptions = {
+                uri: Node + '/mine',
+                method: 'GET',
+                json: true
+               
+            };
+
+            requests.push(reqPromise(requestOptions));
+        });
+
+        Promise.any(requests)
+            .then(newBlock => {
+                res.json(
+                    {
+                        message: `Mining Success!`
+                    }
+                );
+                votechain.addBlock(newBlock);
+                const requests = [];
+                votechain.networkNodes.forEach(networkNode => {
+                    const requestOptions = {
+                        uri: networkNode + '/Postblock',
+                        method: 'POST',
+                        body: {newBlock:newBlock,extra:extra},
+                        json: true
+                    };
+        
+                    requests.push(reqPromise(requestOptions));
+                });
+        
+                Promise.all(requests)
+                    .then(data => {
+                        res.json(
+                            {
+                                message: `Creating and broadcasting block successfully!`
+                            }
+                        );
+                    });
+            });
+            
+           
+           votechain.pendingTransactions=[]; 
+           votechain.pendingTransactions.push(extra);
+           const stop = Date.now();
+           console.log(`Time Taken to execute = ${(stop - start)/1000} seconds`);
+       }
     }
+    
 });
+
+
+
 
 app.post('/register-node', function (req, res) {
     console.log(req.body.nodeUrl);
@@ -179,43 +264,6 @@ app.post('/register-node', function (req, res) {
 
 
 
-app.post('/register-and-broadcast-node', function (req, res) {
-    const nodeUrl = req.body.nodeUrl;
-
-    if (votechain.networkNodes.indexOf(nodeUrl) == -1) {
-        votechain.networkNodes.push(nodeUrl);
-    }
-
-    const registerNodes = [];
-    votechain.networkNodes.forEach(networkNode => {
-        const requestOptions = {
-            uri: networkNode + '/register-node',
-            method: 'POST',
-            body: { nodeUrl: nodeUrl },
-            json: true
-        };
-
-        registerNodes.push(reqPromise(requestOptions));
-    });
-
-    Promise.all(registerNodes)
-        .then(data => {
-            const bulkRegisterOptions = {
-                uri: nodeUrl + '/register-bulk-nodes',
-                method: 'POST',
-                body: { networkNodes: [...votechain.networkNodes, votechain.nodeUrl] },
-                json: true
-            };
-
-            return reqPromise(bulkRegisterOptions);
-        }).then(data => {
-            res.json(
-                {
-                    message: 'A node registers with network successfully!'
-                }
-            );
-        });
-});
 
 app.post('/register-bulk-nodes', function (req, res) {
     const networkNodes = req.body.networkNodes;
@@ -253,14 +301,14 @@ app.get('/consensus', function (req, res) {
             let longestChain = null;
             let pendingTransactions = null;
             let difficulty=null;
-            let maxTransperblock=null;
+            let maxvotes=null;
             blockchains.forEach(blockchain => {
                 if (blockchain.chain.length > maxChainLength) {
                     maxChainLength = blockchain.chain.length;
                     longestChain = blockchain.chain;
                     pendingTransactions = blockchain.pendingTransactions;
                     difficulty=blockchain.difficulty;
-                   maxTransperblock= blockchain.maxTransperblock;
+                   maxvotes= blockchain.maxvotes;
                 }
             });
 
@@ -275,7 +323,7 @@ app.get('/consensus', function (req, res) {
                 votechain.chain = longestChain;
                 votechain.pendingTransactions = pendingTransactions;
                 votechain.difficulty=difficulty;
-                votechain.maxTransperblock= maxTransperblock;
+                votechain.maxvotes= maxvotes;
                 res.json({
                     message: 'Chain is updated!',
                     chain: votechain.chain
--- server01/Mainchain.js
+++ server01/Mainchain.js
@@ -1,5 +1,5 @@
 const SHA256=require('crypto-js/sha256');
-const nodeUrl = process.argv[3];  
+const nodeUrl =process.argv[3];  
 class Block{
         //Block constructor 
         constructor(timestamp,Transactions,previousHash=''){
@@ -7,70 +7,59 @@ class Block{
             this.timestamp=timestamp;
             this.Transactions=Transactions;
             this.previousHash=previousHash;
-            this.hash=this.calculateHash();
+            this.hash;
             this.nonce=0;
-            this.miner=0;
+            this.miner='<----------------Creator------------------->';
         }
 
     //hash generation func.
-    calculateHash(){
-        return SHA256(this.previousHash+this.timestamp+this.nonce+JSON.stringify(this.Transactions)).toString();
-    }
+   
 
     //mining difficulty func.
-    mineBlock(difficulty){
-        while(this.hash.substring(0,difficulty)!== Array(difficulty+1).join('0')){
-            this.nonce++;
-            this.hash=this.calculateHash();
-        }
-        console.log("Block mined :"+this.hash);
-    }
+   
 
 }
 
-class Blockchain{
-
+ class Blockchain {
+    
     constructor(){
         this.chain=[this.createGensis()];
         this.pendingTransactions = [];
-        this.difficulty;     //Setting the difficulty
+        this.difficulty;  //Setting the difficulty
         this.nodeUrl = nodeUrl;         
         this.networkNodes = [];
-        this.reward=0;
-        this.maxTransperblock;//setting max amount of transaction per block
-    }  
-    
+        this.maxvotes;//setting max amount of votes per block
+    } 
+
+    mineBlock(addBlock){
+        addBlock.miner=nodeUrl;
+        addBlock.hash='default';
+        while(addBlock.hash.substring(0,this.difficulty)!== Array(this.difficulty+1).join('0')){
+           addBlock.nonce++;
+           addBlock.hash=this.calculateHash(addBlock);
+            
+        }
+      
+        console.log("Block mined :"+addBlock.hash);
+       
+        return addBlock;
+    }
+
+    calculateHash(addBlock){
+        return SHA256(addBlock.previousHash+addBlock.timestamp+addBlock.miner+addBlock.nonce+JSON.stringify(addBlock.Transactions)).toString();
+    } 
+   
     //Genesis Block
     createGensis(){
-        return new Block('IT','Jaival Faisal Bautik','0');
+        
+        const genesisBlock= new Block('IT','Jaival Faisal Bautik','0');
+         genesisBlock.hash=this.calculateHash(genesisBlock);
+         return genesisBlock;
     }
 
     //Returns last block
     getLatestblock(){
         return this.chain[this.chain.length-1];
-    }
-
-    //Forces the  incomplete block
-    ForceTransactionBlock(){
-       
-        this.addBlock(new Block,true);
-        console.log(' Forced-Mining!!!');
-        this.pendingTransactions=[]; 
-    }
-
-    //New block adding once mining is done
-    addBlock(newBLock,state){
-        console.log('state:',state);
-        newBLock.previousHash=this.getLatestblock().hash;
-        newBLock.timestamp=Date.now();
-        if(state==false){
-        newBLock.Transactions=this.pendingTransactions.slice(0,this.maxTransperblock);
-    	}
-        else{
-            newBLock.Transactions=this.pendingTransactions;
-        }
-        newBLock.mineBlock(this.difficulty);
-        this.chain.push(newBLock);
     }
 
     addTransactions(uid,receiver,location,age,gender){
@@ -82,36 +71,53 @@ class Blockchain{
             gender:gender
         }
        return Transactions;
-        //this.PendingTransactions(Transactions);
+        // this.PendingTransactions(Transactions);
      }
-    PendingTransactions(Transactions){
-        let total=this.pendingTransactions.length;
-        if(total<this.maxTransperblock){
-            this.pendingTransactions.push(Transactions);
-        }
-        else{
-            this.pendingTransactions.push(Transactions);
-            //console.log(this.pendingTransactions);
-            const extra=this.pendingTransactions[this.maxTransperblock];
-            //console.log(extra);
-            this.addBlock(new Block,false);
-            console.log('Mining since pending list is full!!!') ;
-           //console.log("chain:"+JSON.stringify(this.chain,null,4) ) ;
-           this.pendingTransactions=[]; 
-           this.pendingTransactions.push(extra);
-           //console.log(this.pendingTransactions);
 
+     createBlock(state){
+        // console.log('state:',state);
+        const newBLock=new Block;
+
+        newBLock.previousHash=this.getLatestblock().hash;
+        newBLock.timestamp=Date.now();
+        if(state==false){
+        newBLock.Transactions=this.pendingTransactions.slice(0,this.maxvotes);
+    	}
+        else{
+            newBLock.Transactions=this.pendingTransactions;
         }
         
+        // newBLock.mineBlock(this.difficulty);
+
+        // console.log("Block  :"+JSON.stringify(newBLock));
+        return newBLock;
+
+        // this.mineBlock(this.difficulty,newBLock)
+        // this.chain.push(newBLock);
     }
+    addBlock(newBlock){
+        this.chain.push(newBlock);
+    }
+
+    // Forces the  incomplete block
+    ForceTransactionBlock(){
+       
+        const addBlock=this.createBlock(true);
+        addBlock.hash=this.calculateHash(addBlock);
+        this.addBlock(addBlock);
+
+        console.log(' Forced-Mining!!! ');
+        this.pendingTransactions=[]; 
+    }
+
+  
     
-    // checkminingstat(hash){
-    //     for(let t=0;t<this.difficulty;t++){
-    //        if(hash!==0){
-    //            return false;
-    //        }
-    //     }
-    // }
+    
+    
+
+    
+    
+   
     isChainValid(){
         const genesisBlock = this.chain[0];
         if ((genesisBlock.timestamp !== 'IT') ||
@@ -127,7 +133,7 @@ class Blockchain{
             const previousblock=this.chain[i-1];
             
            
-            if(currentblock.hash!==currentblock.calculateHash() /*|| checkminingstat(currentblock.hash)*/){
+            if(currentblock.hash!==this.calculateHash(currentblock) ){
                 return false;
             }
 
@@ -140,23 +146,21 @@ class Blockchain{
 
     //Exisiting uid check function
     DoesVoteExist(uid){
-      //console.log(this.getLatestblock().Transactions);
-      //console.log(this.pendingTransactions);
-      console.log(this.getLatestblock().Transactions=='Jaival Faisal Bautik' );
+  
    
-      console.log(this.pendingTransactions.length==0);
+    //   console.log(this.pendingTransactions.length==0);
         if(this.getLatestblock().Transactions=='Jaival Faisal Bautik' && this.pendingTransactions.length==0){
-            console.log('condition-1');
+            // console.log('condition-1');
             return false;
         }
         else if(this.getLatestblock().Transactions=='Jaival Faisal Bautik' && this.pendingTransactions.length!=0){
-            console.log('condition-2');
+            // console.log('condition-2');
             for(let i=0;i< this.pendingTransactions.length;i++){
                 
                 const current= Object.values(this.pendingTransactions[i]);
-                console.log(current.length);
+                // console.log(current.length);
                
-                    console.log(current[0]);
+                    // console.log(current[0]);
                     if(current[0]==uid){
                         console.log('True uid Exist');
                        
@@ -165,31 +169,14 @@ class Blockchain{
                 
             } 
         }
-        // else if(this.getLatestblock().Transactions!='Jaival Faisal Bautik'&&this.pendingTransactions.length==0){
-        //     console.log('here@does3');
-            
-            
-        //     for(let i=1;i< this.chain.length;i++){
-                
-        //         const currentblock=this.chain[i].Transactions;
-        //         console.log(currentblock);
-        //         for(let j=0;j< currentblock.length;j++){
-                
-        //             if(currentblock[j].uid==uid){
-        //                 console.log('True uid Exist');
-        //                 console.log(currentblock[j].uid);
-        //                 return true;
-        //             }
-        //         }
-        //     }
-        // }
+      
         else if(this.getLatestblock().Transactions!='Jaival Faisal Bautik'&&this.pendingTransactions.length!=0){
-            console.log('condition-4');
+            // console.log('condition-4');
             for(let i=0;i< this.pendingTransactions.length;i++){
                 const current= Object.values(this.pendingTransactions[i]);
-                console.log(current.length);
+                // console.log(current.length);
                
-                    console.log(current[0]);
+                    // console.log(current[0]);
                     if(current[0]==uid){
                         console.log('True uid Exist');
                        
@@ -199,17 +186,18 @@ class Blockchain{
             for(let i=1;i< this.chain.length;i++){
                
                 const currentblock=this.chain[i].Transactions;
-                console.log(currentblock);
+                // console.log(currentblock);
                 for(let j=0;j< currentblock.length;j++){
                 
                     if(currentblock[j].uid==uid){
-                        console.log('True uid Exist');
-                        console.log(currentblock[j].uid);
+                        // console.log('True uid Exist');
+                        // console.log(currentblock[j].uid);
                         return true;
                     }
                 }
             }
         }
+        return false;
             
     }
     Results(){
@@ -219,7 +207,7 @@ class Blockchain{
             
             for(let j=0;j< currentblock.length;j++){
                 uid=uid.currentblock[j];
-            receiver=receiver.currentblock[j];
+                receiver=receiver.currentblock[j];
                 return(uid,receiver);
             }
 
@@ -227,68 +215,7 @@ class Blockchain{
     }
 
 }
-/*let ch= new Blockchain();
-console.log('mining block....');
-ch.addBlock(new Block('s123','19/07',{test:1}));
-console.log('mining block....');
-ch.addBlock(new Block('s123','19/07',{test:2}));
-console.log('is chain valid :'+ch.isChainvalid());*/
-/*ch.chain[1].data={test:22};
-console.log('is chain valid :'+ch.isChainvalid());
-console.log(JSON.stringify(ch, null,4));
-*/
+
 
 module.exports = Blockchain;
 
-
-// let x= new Blockchain();
-
-// x.addTransactions('1','b','c','d','e');
-// x.addTransactions('2','b','c','d','e');
-// x.addTransactions('3','b','c','d','e');
-// x.addTransactions('4','b','c','d','e');
-// x.addTransactions('5','b','c','d','e');
-// x.addTransactions('6','b','c','d','e');
-// x.addTransactions('7','b','c','d','e');
-// x.addTransactions('8','b','c','d','e');
-// x.addTransactions('9','b','c','d','e');
-// x.addTransactions('10','b','c','d','e');
-// x.addTransactions('11','b','c','d','e');
-
-
-
-
-
-
-
-
-// x.addTransactions('xav','12','c','d','e');
-// x.addTransactions('xav','13','c','d','e');
-// x.addTransactions('xav','14','c','d','e');
-// x.addTransactions('xav','15','c','d','e');
-// x.addTransactions('xav','16','c','d','e');
-// x.addTransactions('xav','17','c','d','e');
-// x.addTransactions('xav','18','c','d','e');
-// x.addTransactions('xav','19','c','d','e');
-// x.addTransactions('xav','20','c','d','e');
-// x.addTransactions('xav','21','c','d','e');
-// x.addTransactions('xav','22','c','d','e');
-
-
-
-
-
-
-
-
-
-
-
-
-// //console.log('Pending'+x.pendingTransactions);
-// //console.log('Chain:'+JSON.stringify(x.chain,null,4));
-// for(i=1;i<x.chain.length;i++){
-    
-//     //x.Results( x.chain[i].Transactions);
-// }
-// x.DoesVoteExist('xav')
